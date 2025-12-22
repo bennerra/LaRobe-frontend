f8982139494055537219a3b16cbc705795ad007d
ELGINA <anstasiaelgina738@gmail.com>
2025-12-22 15:05:21 +0400

    First commit

---
+++ src/constants/paths.ts
@@ -0,0 +1,25 @@
+export const enum AppRoutes {
+  MAIN = "/",
+  AUTH = "/auth",
+  REGISTRATION = "/registration",
+  PROFILE = "/profile",
+  ABOUT = "/about",
+  CATALOG = "/catalog",
+  PRODUCT_DETAIL = "/product",
+  NOT_FOUND = "*",
+}
+
+export const NotAuthPaths: AppRoutes[] = [
+  AppRoutes.REGISTRATION,
+  AppRoutes.AUTH,
+  AppRoutes.NOT_FOUND,
+  AppRoutes.ABOUT,
+  AppRoutes.CATALOG,
+  AppRoutes.PRODUCT_DETAIL,
+];
+
+export const AuthPaths: AppRoutes[] = [
+  AppRoutes.MAIN,
+  AppRoutes.PROFILE,
+  AppRoutes.NOT_FOUND,
+];
